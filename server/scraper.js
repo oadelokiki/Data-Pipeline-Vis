f5e017f1d32f1de1261c3c5b67329d8bb5acce6d
ranktel <ranktel@worktop.myguest.virtualbox.org>
2023-04-11 09:55:08 -0400

    scraperaper can pull data, but is slow and bloated

--- server/scraper.js
+++ server/scraper.js
@@ -17,7 +17,9 @@ const siteList = require("../siteList.json");
 const fetch = require("node-fetch")
 const extractUrls = require("extract-urls")
 const parse = require ('html-dom-parser')
-const jsdom = require("jsdom");
+const {JSDOM}  = require("jsdom");
+//web crawler
+const puppeteer = require("puppeteer")
 
 async function getAllSubDomains(){
 	//should scrape the sitemap_index.xml for all of the sites.xml(s)
@@ -58,32 +60,70 @@ async function getAllSubDomains(){
 
 }
 
-
+//web scraper (scrapes from crawler results)
 async function scrapeAdPlacements(){
+
 	
 	const subdomains = await getAllSubDomains();
+	console.log("Here's a cleaned list of all subdomains that have actual html page content.")
+	console.log(subdomains);
 	//Array of subdomains from the main site.
+	var exampleCounter = 0;
 
 	for(let i = 0; i < subdomains.length; i++){
+/*
 		const site = await fetch(subdomains[i]);
 		const siteHTML = await site.text();
 		
-		//next I have to parse the actual html of all of these sites, and find out what I need data-wise from each ad placement.
-		//this is the part where I SHOULD put the data into a sequelize model, but I'll fully load in memory, and realease the long list of subdomains once it's all loaded.
-
-
-		const dom = new jsdom.JSDOM(siteHTML);
-		let adPlacementsByType = []
-	
-		for(let r = 0 ; r < siteList["AdPlacementTypes"].length; r++){
-			const elementList = dom.window.document.querySelector(`[id^="${siteList["AdPlacementTypes"][r]}"]`)
-			console.log(elementList);		
-		}
-
 		
+	
+
 		const htmlDoc = parse(siteHTML);
 
-		
+		//does this refer to the way that I actually see the element in the DOM
+		for(let j = 0; j < htmlDoc.length; j++){
+			console.log(htmlDoc[j]);
+		}
+		break;
+		//console.log(siteHTML);			
+	*/
+		try{
+		( async () => {
+
+			const browser = await puppeteer.launch();
+        	          const page = await browser.newPage();
+				await page.setDefaultNavigationTimeout(0);	
+			  await page.goto(subdomains[i]);
+
+			  // Set screen size
+			await page.setViewport({width: 1080, height: 1024});
+			const data = await page.evaluate( () => {
+				const siteList = {"AdPlacementTypes":["AdThrive", "google_ads"]}
+
+				const divs = [];
+			
+			for(let ads = 0 ; ads < siteList["AdPlacementTypes"].length; ads++){
+					let datum = document.querySelectorAll("[id *= AdThrive]");
+					let datum2 = document.querySelectorAll("[id *= google_ads]")
+					let datum3 = document.querySelectorAll("[id *= ad]")
+
+					divs.push(datum);
+					divs.push(datum2);
+				}
+
+			return divs;
+			})
+		  	
+		  	await browser.close();
+			console.log(data);
+	
+		})();
+			if (i == 2){
+				break;
+			}
+		}catch(err){
+			continue;
+		}
 	}
 }
 
